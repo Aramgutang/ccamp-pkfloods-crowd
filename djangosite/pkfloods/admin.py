c4a48af37ca61747b39d7a2b7f1cbe946ac879b6
Aram Dulyan <aram00@gmail.com>
2010-09-05 14:28:59 +1000

    Added some models for the processed data.

---
+++ djangosite/pkfloods/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from models import *
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+class ActionableAdmin(admin.ModelAdmin):
+    pass
+
+class DamageAssessmentAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('', {
+            'fields': ('sms', 'still_flooded',)
+        }),
+        ('Location', {
+            'fields': ('text_location', 'location',)
+        }),
+        ('Population', {
+            'fields': ('text_population', 'population_total', 'population_adults', 'population_children')
+        }),
+        ('Houses', {
+            'fields': ('text_houses', 'publication_date', 'status')
+        }),
+        ('Other losses', {
+            'fields': ('text_other_losses', 'lost_roads', 'lost_livestock', 'lost_schools', 'lost_crops_acres', 'lost_crops_percentage')
+        }),
+    )
+    
+    readonly_fields = ('sms', 'text_location', 'text_population', 'text_houses', 'text_other_losses')
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Actionable, ActionableAdmin)
+admin.site.register(DamageAssessment, DamageAssessmentAdmin)
--- djangosite/pkfloods/models.py
+++ djangosite/pkfloods/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+from crowdflower.models import SMS
+
+LANGUAGES = ['English', 'Urdu', 'Pashtun', 'Other',]
+
+class UnifiedCouncil(models.Model):
+    name = models.CharField(max_length=180)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class Village(models.Model):
+    name = models.CharField(max_length=180)
+    unified_council = models.ForeignKey(UnifiedCouncil, null=True)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class Location(models.Model):
+    village = models.ForeignKey(Village, null=True)
+    unified_council = models.ForeignKey(UnifiedCouncil, null=True)
+    
+    def __unicode__(self):
+        if self.unified_council:
+            return '%s, UC %s' % (self.village, self.unified_council)
+        else:
+            return self.village.name
+
+class Actionable(models.Model):
+    sms = models.ForeignKey(SMS)
+    language = models.CharField(max_length=180, choices=zip(LANGUAGES, LANGUAGES))
+    junk_text = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return self.sms.sms
+
+class DamageAssessment(models.Model):
+    sms = models.ForeignKey(SMS)
+    
+    text_location = models.CharField(max_length=180, blank=True)
+    text_population = models.CharField(max_length=180, blank=True)
+    text_houses = models.CharField(max_length=180, blank=True)
+    text_other_losses = models.CharField(max_length=180, blank=True)
+    text_notes = models.CharField(max_length=180, blank=True)
+    
+    location = models.ForeignKey(Location, null=True)
+    
+    population_adults = models.IntegerField(null=True, blank=True)
+    population_children = models.IntegerField(null=True, blank=True)
+    
+    houses_destroyed = models.IntegerField(null=True, blank=True)
+    houses_damaged = models.IntegerField(null=True, blank=True)
+    
+    lost_roads = models.IntegerField(null=True, blank=True, help_text='in km.')
+    lost_livestock = models.CharField(max_length=180, blank=True)
+    lost_schools = models.IntegerField(null=True, blank=True)
+    lost_crops_acres = models.IntegerField(null=True, blank=True)
+    lost_crops_percentage = models.IntegerField(null=True, blank=True)
+    
+    still_flooded = models.NullBooleanField()
+    
+    def __unicode__(self):
+        return self.sms.sms
